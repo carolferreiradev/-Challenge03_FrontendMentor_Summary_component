03e9ea743b3f140850222ec0f04f039222535806
Carol Ferreira <developer.anacarolina@gmail.com>
2022-05-27 17:17:16 -0300

    feat: add styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,25 +1,27 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
-    colors: {
-        blue: {
-            "900": "hsl(223, 47%, 23%)",
-            "800": "hsl(224, 23%, 55%)",
-            "700": "hsl(245, 75%, 52%)",
-            "600": "hsl(225, 100%, 94%)",
-            "500": "hsl(225, 100%, 98%)",
-        }
+  colors: {
+    blue: {
+      "900": "hsl(223, 47%, 23%)",
+      "800": "hsl(224, 23%, 55%)",
+      "700": "hsl(245, 75%, 52%)",
+      "750": "hsl(245, 75%, 60%)",
+      "600": "hsl(225, 100%, 94%)",
+      "500": "hsl(225, 100%, 98%)",
+      "400": "hsl(225, 100%, 96%)",
     },
-    fonts: {
-        heading: 'Red Hat Display',
-        body: 'Red Hat Display'
+  },
+  fonts: {
+    heading: "Red Hat Display",
+    body: "Red Hat Display",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "blue.600",
+        color: "blue.800",
+      },
     },
-    styles:{
-        global:{
-            body:{
-                bg:'blue.600',
-                color:'blue.800'
-            }
-        }
-    }
-})
+  },
+});
