74e63ab7b525309571efcf17cadbea948f59a7ee
Carol Ferreira <developer.anacarolina@gmail.com>
2022-05-27 17:17:35 -0300

    feat: add lang and description tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { NextScript, Html, Head, Main } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -11,7 +11,15 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@500;700;900&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/favicon-32x32.png" type="image/png" />
+          <link
+            rel="shortcut icon"
+            href="/favicon-32x32.png"
+            type="image/png"
+          />
+          <meta
+            name="description"
+            content="Order Summary: You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!"
+          />
         </Head>
         <body>
           <Main />
